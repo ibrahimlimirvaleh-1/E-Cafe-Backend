6d5fb0a4398efa93abfd3b3e190cb0102e43c4fb
mirvaleh.ibrahimli <mirvaleh.ibrahimli@BACKEND-06>
2026-09-02 15:09:32 +0400

    Fix owner search user response

--- src/ECafe.Application/DTOs/User/GetAllUserResponseDto.cs
+++ src/ECafe.Application/DTOs/User/GetAllUserResponseDto.cs
@@ -1,4 +1,4 @@
-﻿namespace ECafe.Application.DTOs.User
+namespace ECafe.Application.DTOs.User
 {
     public class GetAllUserResponseDto
     {
@@ -7,9 +7,17 @@
 
         public string Surname { get; set; } = null!;
 
+        public string Email { get; set; } = null!;
+
+        public string Phone { get; set; } = null!;
+
         public bool IsActive { get; set; }
 
         public decimal? Rating { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; } = null!;
 
         public RoleDto Role { get; set; } = null!;
 
